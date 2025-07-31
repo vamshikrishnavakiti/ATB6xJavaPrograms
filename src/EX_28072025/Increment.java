a90c532e2d5022b70179c63f70e1739ae8adf551
Vamshi Krishna <vamshi.qaeng@gmail.com>
2025-07-31 07:16:57 +0530

    increment

---
+++ src/EX_28072025/Increment.java
@@ -0,0 +1,12 @@
+package EX_28072025;
+
+public class Increment {
+    public static void main(String[] args) {
+        int a = 10;
+        System.out.println(++a);
+        System.out.println(++a);
+        System.out.println(a++);
+        System.out.println(a);
+
+    }
+}
